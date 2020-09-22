f44b40c59f859604cbeea9b13192abea2d4c0340
Siamak Salimi <ssalimi1220@gmail.com>
2020-09-22 15:26:20 -0400

    data passes from location.dart to loading screen and shown on the screen as variables

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -30,13 +30,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     longitude = location.longitude;
 
     NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=imperial');
 
     var weatherData = await networkHelper.getData();
 
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen();
-    }));
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return LocationScreen(
+          locationWeather: weatherData,
+        );
+      }),
+    );
   }
 
   @override
